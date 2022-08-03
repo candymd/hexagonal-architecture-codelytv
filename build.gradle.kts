678402f02e18685590a1079a81eeecf0b71e1fa2
Candy Montero <candymdeoleo@gmail.com>
2022-08-03 15:07:05 +0200

    add gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,10 +17,16 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.mockito:mockito-inline:4.6.1")
+    testImplementation("org.mockito:mockito-junit-jupiter:4.6.1")
+    testImplementation("org.assertj:assertj-core:3.23.1")
+    testImplementation("io.rest-assured:spring-mock-mvc:5.1.1")
+
 }
 
 tasks.withType<KotlinCompile> {
